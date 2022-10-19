32b4471f26615199d43ee6c46f55ab8a89f616ad
postmannen <postmannen@gmail.com>
2022-10-19 11:15:43 +0200

    update

--- create.sh
+++ create.sh
@@ -11,5 +11,6 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 echo "$SCRIPT_DIR"
 
 wget https://github.com/prometheus/node_exporter/releases/download/v$version/$name-$version.linux-amd64.tar.gz -P "$SCRIPT_DIR"
+mkdir -p "$SCRIPT_DIR"/binaries
 tar xzvf "$SCRIPT_DIR"/$name-$version.linux-amd64.tar.gz -C "$SCRIPT_DIR"
-cp -a "$SCRIPT_DIR"/$name-$version.linux-amd64/node_exporter "$SCRIPT_DIR"
+mv "$SCRIPT_DIR"/$name-$version.linux-amd64/node_exporter "$SCRIPT_DIR"/binaries/$name
